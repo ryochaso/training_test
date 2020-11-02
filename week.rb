099f8d3de7b6ddce14d06b2cd35780fb1ef2b757
ryosuke <64516382+ryochaso@users.noreply.github.com>
2020-11-02 19:11:01 +0900

    Issue6

---
+++ week.rb
@@ -0,0 +1,21 @@
+def getWeek
+  wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+
+  # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
+  @todays_date = Date.today
+  # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
+
+  @week_days = []
+
+  plans = Plan.where(date: @todays_date..@todays_date + 6)
+
+  7.times do |x|
+    today_plans = []
+    plan = plans.map do |plan|
+      today_plans.push(plan.plan) if plan.date == @todays_date + x
+    end
+    
+    days = { :month => (@todays_date + x).month, :date => @todays_date.day + x, :plans => today_plans, wday: wdays[@todys_date.wday]}
+    @week_days.push(days)
+  end
+end
