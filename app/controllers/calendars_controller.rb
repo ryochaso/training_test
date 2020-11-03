e956c452f21156ffce0dbc0de549ad7f9ea45076
ryosuke <64516382+ryochaso@users.noreply.github.com>
2020-11-03 15:43:17 +0900

    Isuue　4~6修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -6,10 +6,8 @@ class CalendarsController < ApplicationController
     @plan = Plan.new
   end
 
-  # 予定の保存
   def create
     
-    #binding.pry
     
     Plan.create(plan_params)
     redirect_to action: :index
@@ -25,10 +23,9 @@ class CalendarsController < ApplicationController
 
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
-    # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
+    
     @todays_date = Date.today
-    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
-
+    
     @week_days = []
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
@@ -40,11 +37,11 @@ class CalendarsController < ApplicationController
       end
 
       wday_num = Date.today.wday + x
-      if wday_num > x
+      if wday_num >= 7
         wday_num = wday_num - 7
       end
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[wday_num] }
+      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[wday_num] }#wday_numで添字として中の配列を取得
       @week_days.push(days)
     end
 
